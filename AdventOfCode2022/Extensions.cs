76587a990f7584f6b4f96f75727c7333ed640440
SebaldDM13 <64290663+SebaldDM13@users.noreply.github.com>
2022-12-03 13:19:27 -0600

    Day 03

---
+++ AdventOfCode2022/Extensions.cs
@@ -0,0 +1,94 @@
+﻿namespace AdventOfCode2022;
+
+public static class Extensions
+{
+    public static IEnumerable<IEnumerable<string>> Partitions(this IEnumerable<string> lines)
+    {
+        IEnumerator<string> enumerator = lines.GetEnumerator();
+        while(enumerator.MoveNext())
+        {
+            yield return NextPartition(enumerator);
+        }
+    }
+
+    private static IEnumerable<string> NextPartition(IEnumerator<string> enumerator)
+    {
+        while (enumerator.Current != string.Empty)
+        {
+            yield return enumerator.Current;
+            if (!enumerator.MoveNext())
+                yield break;
+        }
+    }
+
+    public static int Total(this IEnumerable<string> lines)
+    {
+        return lines.Sum(int.Parse);
+    }
+
+    public static IEnumerable<int> Totals(this IEnumerable<IEnumerable<string>> partitions)
+    {
+        foreach (var partition in partitions)
+        {
+            yield return partition.Total();
+        }
+    }
+
+    public static IEnumerable<char> CommonCharacters(this IEnumerable<string> strings)
+    {
+        IEnumerator<string> enumerator = strings.GetEnumerator();
+        enumerator.MoveNext();
+        IEnumerable<char> common = enumerator.Current;
+        while(enumerator.MoveNext())
+        {
+            common = common.Intersect(enumerator.Current);
+        }
+
+        return common;
+    }
+
+    public static IEnumerable<IEnumerable<char>> CommonCharacters(this IEnumerable<IEnumerable<string>> partitions)
+    {
+        foreach(IEnumerable<string> strings in partitions)
+        {
+            yield return strings.CommonCharacters();
+        }
+    }
+
+    public static IEnumerable<T> Singles<T>(this IEnumerable<IEnumerable<T>> partitions)
+    {
+        foreach(IEnumerable<T> p in partitions)
+        {
+            yield return p.Single();
+        }
+    }
+
+    public static IEnumerable<string> Halves(this string s)
+    {
+        yield return s[..(s.Length / 2)];
+        yield return s[(s.Length / 2)..];
+    }
+
+    public static IEnumerable<IEnumerable<string>> Halves(this IEnumerable<string> lines)
+    {
+        foreach(string line in lines)
+        {
+            yield return line.Halves();
+        }
+    }
+
+    public static int PriorityValue(this char c) => c switch
+    {
+        >= 'a' and <= 'z' => c - 'a' + 1,
+        >= 'A' and <= 'Z' => c - 'A' + 27,
+        _ => 0
+    };
+
+    public static IEnumerable<int> PriorityValues(this IEnumerable<char> chars)
+    {
+        foreach (char c in chars)
+        {
+            yield return c.PriorityValue();
+        }
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,17 +1,26 @@
 ﻿using AdventOfCode2022;
+using System.Xml.Schema;
 
 const string path = "Input";
 IEnumerable<string> lines;
 Console.WriteLine("Advent of Code 2022");
 Console.WriteLine();
+
 Console.WriteLine("Day 01:");
 lines = File.ReadLines(Path.Combine(path, "Day01.txt"));
-Console.WriteLine("Most Calories total: " + GroupAdder.CalculateTotals(lines, string.Empty).Max());
-Console.WriteLine("Sum of top three Calorie totals: " + GroupAdder.CalculateTotals(lines, string.Empty).OrderDescending().Take(3).Sum());
+Console.WriteLine("Most Calories total: " + lines.Partitions().Totals().Max());
+Console.WriteLine("Sum of top three Calorie totals: " + lines.Partitions().Totals().OrderDescending().Take(3).Sum());
 Console.WriteLine();
+
 Console.WriteLine("Day 02:");
 lines = File.ReadLines(Path.Combine(path, "Day02.txt"));
-Console.WriteLine("Part 1 score (switch implementation): " + lines.Select(line => RockPaperScissorsSwitchImplementation.ScoreOfOpponentMoveAndPlayerMove(line)).Sum());
-Console.WriteLine("Part 2 score (switch implementation): " + lines.Select(line => RockPaperScissorsSwitchImplementation.ScoreOfOpponentMoveAndResult(line)).Sum());
-Console.WriteLine("Part 1 score (equation implementation): " + lines.Select(line => RockPaperScissorsEquationImplementation.ScoreOfOpponentMoveAndPlayerMove(line)).Sum());
-Console.WriteLine("Part 2 score (equation implementation): " + lines.Select(line => RockPaperScissorsEquationImplementation.ScoreOfOpponentMoveAndResult(line)).Sum());
+Console.WriteLine("Part 1 score (switch implementation): " + lines.Sum(RockPaperScissorsSwitchImplementation.ScoreOfOpponentMoveAndPlayerMove));
+Console.WriteLine("Part 2 score (switch implementation): " + lines.Sum(RockPaperScissorsSwitchImplementation.ScoreOfOpponentMoveAndResult));
+Console.WriteLine("Part 1 score (equation implementation): " + lines.Sum(RockPaperScissorsEquationImplementation.ScoreOfOpponentMoveAndPlayerMove));
+Console.WriteLine("Part 2 score (equation implementation): " + lines.Sum(RockPaperScissorsEquationImplementation.ScoreOfOpponentMoveAndResult));
+Console.WriteLine();
+
+Console.WriteLine("Day 03:");
+lines = File.ReadLines(Path.Combine(path, "Day03.txt"));
+Console.WriteLine("Priority sum of common items: " + lines.Halves().CommonCharacters().Singles().PriorityValues().Sum());
+Console.WriteLine("Priority sum of badges: " + lines.Chunk(3).CommonCharacters().Singles().PriorityValues().Sum());
