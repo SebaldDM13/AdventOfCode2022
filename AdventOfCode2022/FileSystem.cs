19f7654b1cd23c7692d25d372e14e7f2763e5665
SebaldDM13 <64290663+SebaldDM13@users.noreply.github.com>
2022-12-07 15:42:40 -0600

    Day 07 refactor

---
+++ AdventOfCode2022/FileSystem.cs
@@ -0,0 +1,58 @@
+﻿namespace AdventOfCode2022;
+
+public class FileSystem
+{
+    public List<DirectoryNode> AllDirectories { get; } = new List<DirectoryNode>();
+
+    public void Initialize(IEnumerable<string> lines)
+    {
+        AllDirectories.Clear();
+        AllDirectories.Add(new("/"));
+        DirectoryNode current = AllDirectories[0];
+        bool isInListing = false;
+
+        foreach (string line in lines)
+        {
+            string[] tokens = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (tokens[0] == "$")
+            {
+                isInListing = false;
+                if (tokens[1] == "ls")
+                {
+                    isInListing = true;
+                }
+                if (tokens[1] == "cd")
+                {
+                    if (tokens[2] == "/")
+                    {
+                        current = AllDirectories[0];
+                    }
+                    else if (tokens[2] == "..")
+                    {
+                        if (current.Parent is not null)
+                        {
+                            current = current.Parent;
+                        }
+                    }
+                    else
+                    {
+                        current = current.SubDirectory(tokens[2]);
+                    }
+                }
+            }
+            else if (isInListing)
+            {
+                if (tokens[0] == "dir")
+                {
+                    DirectoryNode subDirectory = new DirectoryNode(tokens[1]);
+                    current.Add(subDirectory);
+                    AllDirectories.Add(subDirectory);
+                }
+                else
+                {
+                    current.Add(new FileNode(tokens[1], int.Parse(tokens[0])));
+                }
+            }
+        }
+    }
+}
